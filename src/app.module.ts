a382b87e6bfb97c3654e7463301c82625798b53c
Binod Gautam <gautambinod629@gmail.com>
2025-06-24 11:42:38 +0545

    configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true
+    }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    AuthModule
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,35 @@
+import {ConfigModule, ConfigService} from "@nestjs/config"
+import {TypeOrmModule, TypeOrmModuleAsyncOptions, TypeOrmModuleOptions} from '@nestjs/typeorm'
+import { DataSource, DataSourceOptions } from "typeorm"
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+    type: 'mysql',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_DATABASE'),
+    autoLoadEntities: true,
+    synchronize: configService.get('NODE_ENV') === 'development',
+    logging: configService.get('NODE_ENV') === 'development',
+  }),
+  inject: [ConfigService],
+};
+
+
+export const dataSourceOptions:DataSourceOptions={
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '3306'),
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'nestjs_app',
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
+  synchronize: false,
+}
+
+const dataSource=new DataSource(dataSourceOptions)
+
+export default dataSource;
