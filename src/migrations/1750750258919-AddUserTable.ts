fcf26dcc9c895f1a869ba3499efcf7c01c8701c5
Binod Gautam <gautambinod629@gmail.com>
2025-06-25 18:51:37 +0545

    add user migration

--- src/migrations/1750750258919-AddUserTable.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class AddUserTable1750750258919 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
--- src/migrations/1750778254905-refreshnullable.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class Refreshnullable1750778254905 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
