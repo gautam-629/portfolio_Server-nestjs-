3142c4e8be4d4af071200f920d8a2196d60fb595
Binod Gautam <gautambinod629@gmail.com>
2025-07-01 19:49:56 +0545

    config swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { GlobalResponseInterceptor } from './common/interceptors/global-response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger=new Logger('Bootstrap')
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -19,6 +22,22 @@ async function bootstrap() {
   app.useGlobalInterceptors(new GlobalResponseInterceptor(reflector));
   app.useGlobalFilters(new GlobalExceptionFilter());
 
-  await app.listen(process.env.PORT ?? 5000);
+
+  const connfig=new DocumentBuilder()
+  .setTitle('Blog Application')
+  .setDescription('This is Api Description')
+  .setVersion('1.0')
+  .addTag('blog')
+  .build()
+
+  const documentFactory=()=>SwaggerModule.createDocument(app,connfig)
+  SwaggerModule.setup('api', app, documentFactory)
+
+  const port=process.env.PORT ?? 5000
+
+  await app.listen(port);
+
+  logger.log(`Application is running on: http://localhost:${port}`);
+  logger.log(`Swagger documentation: http://localhost:${port}/api`);
 }
 bootstrap();
